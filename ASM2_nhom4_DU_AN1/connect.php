4b152106bb651bcef7c58531a7d8f11323181646
hanvn136 <han.070419@gmail.com>
2022-12-06 15:09:24 +0700

    .

--- ASM2_nhom4_DU_AN1/connect.php
+++ ASM2_nhom4_DU_AN1/connect.php
@@ -2,10 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "du_an_mau"; // dat ten gi thi lam on de vao gium
+$database = "du_an_1"; // dat ten gi thi lam on de vao gium
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=du_an_mau", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=du_an_1", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Kết nối thành công";
